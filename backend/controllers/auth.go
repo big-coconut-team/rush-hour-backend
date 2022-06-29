ac48250de502e5eea36f532300afcf20dd9ff32a
ZestOfLife <jamescook371@gmail.com>
2022-06-29 14:09:11 +0900

    Add login route

--- backend/controllers/auth.go
+++ backend/controllers/auth.go
@@ -2,19 +2,49 @@ package controllers
 
 import (
 	"net/http"
-  	"github.com/gin-gonic/gin"
 	"scalable-final-proj/backend/models"
 
+	"github.com/gin-gonic/gin"
 )
+
+type LoginInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func Login(c *gin.Context) {
+
+	var input LoginInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	u := models.User{}
+
+	u.Username = input.Username
+	u.Password = input.Password
+
+	token, err := models.LoginCheck(u.Username, u.Password)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username or password is incorrect."})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
+
+}
 
 type RegisterInput struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
-	Email string `json:"email" binding:"required"`
+	Email    string `json:"email" binding:"required"`
 }
 
-func Register(c *gin.Context){
-	
+func Register(c *gin.Context) {
+
 	var input RegisterInput
 
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -29,13 +59,13 @@ func Register(c *gin.Context){
 	u.Email = input.Email
 	u.Coin = 0
 
-	_,err := u.SaveUser()
+	_, err := u.SaveUser()
 
-	if err != nil{
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message":"registration success"})
+	c.JSON(http.StatusOK, gin.H{"message": "registration success"})
 
 }
