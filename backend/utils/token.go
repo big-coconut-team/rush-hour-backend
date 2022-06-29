fa238d9cf58ee7adce3d2639fe72ab39550562d0
ZestOfLife <jamescook371@gmail.com>
2022-06-29 13:59:01 +0900

    Add token generator for session bookeeping

---
+++ backend/utils/token.go
@@ -0,0 +1,83 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+// Make sure to put token signature as enviorment variable
+// once done testing
+
+func GenerateToken(uid uint) (string, error) {
+
+	if err != nil {
+		return "", err
+	}
+
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["uid"] = uid
+	// 6 hours per login session
+	// if you want to persist login
+	// do it from frontend
+	claims["exp"] = time.Now().Add(time.Hour * time.Duration(6)).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString("Writeyourownsignaturehere")
+
+}
+
+func TokenValid(c *gin.Context) error {
+	tokenString := ExtractToken(c)
+	_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("Writeyourownsignaturehere"), nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ExtractToken(c *gin.Context) string {
+	token := c.Query("token")
+	if token != "" {
+		return token
+	}
+	bearerToken := c.Request.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1]
+	}
+	return ""
+}
+
+func ExtractTokenID(c *gin.Context) (uint, error) {
+
+	tokenString := ExtractToken(c)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("Writeyourownsignaturehere"), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		uid, err := strconv.ParseInt(claims["uid"], 10, 11)
+		if err != nil {
+			return 0, err
+		}
+		return uint(uid), nil
+	}
+	return 0, nil
+}
