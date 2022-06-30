bb6d1283ace347ea96be9b38e4b84b9f226a7cb0
tnapat2000 <tnapat2000@gmail.com>
2022-06-30 21:30:56 +0700

    rearranged lines

--- backend/controllers/profile_con.go
+++ backend/controllers/profile_con.go
@@ -2,10 +2,10 @@ package controllers
 
 import (
 	"net/http"
-	"scalable-final-proj/backend/models"
 	"scalable-final-proj/backend/utils"
 
 	"github.com/gin-gonic/gin"
+	"errors"
 )
 
 func CurrentUser(c *gin.Context) {
@@ -17,7 +17,7 @@ func CurrentUser(c *gin.Context) {
 		return
 	}
 
-	u, err := models.GetUserByID(uint(user_id))
+	u, err := GetUserByID(uint(user_id))
 
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -25,4 +25,18 @@ func CurrentUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "success", "data": u})
+}
+
+func GetUserByID(uid uint) (User, error) {
+
+	var u User
+
+	if err := DB.First(&u, uid).Error; err != nil {
+		return u, errors.New("User not found!")
+	}
+
+	u.Password = ""
+
+	return u, nil
+
 }
