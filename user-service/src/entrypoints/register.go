1404423f98ae05d308b9dceddecd458038cf7b9b
ZestOfLife <jamescook371@gmail.com>
2022-07-02 18:46:45 +0900

    Add register entrypoint

---
+++ user-service/src/entrypoints/register.go
@@ -0,0 +1,38 @@
+package entrypoints
+
+import (
+	"user-service/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+type RegisterInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+}
+
+func Register(c *gin.Context) {
+	var input RegisterInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	u := models.User{}
+
+	u.Username = input.Username
+	u.Password = input.Password
+	u.Email = input.Email
+	u.Coin = 0
+
+	_, err := u.SaveUser()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "registration success"})
+}
