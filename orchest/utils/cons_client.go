372d02851c5858db91d45b1a3a5f06ae71346073
tnapat2000 <tnapat2000@gmail.com>
2022-07-07 03:10:19 +0700

    had problem in SendMSGPayment

--- orchest/utils/cons_client.go
+++ orchest/utils/cons_client.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-
+	"strings"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
@@ -13,20 +13,16 @@ func ListenOrder() {
 		"bootstrap.servers":               "localhost:9092",
 		"group.id":                        "order-group",
 		"go.application.rebalance.enable": true,
-
-		// "client.id": "localhost",
-		// "acks": "all"
 	})
 	if err != nil {
 		log.Panic(err)
 	}
 
-
 	var topics []string
-	topics = append(topics, "order")
-	err2 := consumer.SubscribeTopics(topics, nil)
+	topics = append(topics, "order", "payment")
+	err = consumer.SubscribeTopics(topics, nil)
 
-	if err2 != nil {
+	if err != nil {
 		log.Panic(err)
 	}
 
@@ -36,8 +32,23 @@ func ListenOrder() {
 		ev := consumer.Poll(0)
 		switch e := ev.(type) {
 		case *kafka.Message:
-			fmt.Printf("%% Message on %s:\n%s\n",
-				e.TopicPartition, string(e.Value))
+			//  Message on order[0]@4:
+			fmt.Printf("%% Message on %s:\n%s\n", e.TopicPartition, string(e.Value))
+			
+			messageChan := fmt.Sprintf("%s",e.TopicPartition)
+
+			// order => payment
+			if strings.Contains(messageChan, "order") {
+				fmt.Println("order")
+			}
+			// payment => stock
+			if strings.Contains(messageChan, "payment") {
+				fmt.Println("payment")
+			}	
+			// else {
+			// 	fmt.Println("topic doesn't exist")
+			// }	
+
 		case kafka.PartitionEOF:
 			fmt.Printf("%% Reached %v\n", e)
 		case kafka.Error:
--- orchest/utils/messages.go
+++ orchest/utils/messages.go
@@ -44,3 +44,40 @@ func StartOrder(c *gin.Context) {
 		log.Panic(err)
 	}
 }
+
+type CPInput struct {
+    	// gorm.Model
+	PaymentID 		int 	`json:"payment_id" binding:"required"`
+	MadeByUserID   	int 	`json:"made_by_id" binding:"required"`
+	Amount			int		`json:"amount" binding:"required"`
+	Paid			bool	`json:"paid" binding:"required"`
+}
+
+func SendMSGPayment(c *gin.Context) {
+
+    var input CPInput
+
+    err := c.ShouldBindJSON(&input)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+	topic := "payment"
+	value, err := json.Marshal(&input)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	delivery_chan := make(chan kafka.Event, 10000)
+	err = Getp_client().Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          []byte(value)},
+		delivery_chan,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+}
