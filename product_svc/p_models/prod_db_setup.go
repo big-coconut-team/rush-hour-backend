81ef64f2081e22375d9fbcb1beeacf6b83fe202a
pearnatnicha <pearpatt2001@gmail.com>
2022-07-11 02:25:32 +0700

    add .env for prod_svc

--- product_svc/p_models/prod_db_setup.go
+++ product_svc/p_models/prod_db_setup.go
@@ -1,43 +1,43 @@
 package p_models
 
 import (
-	// "fmt"
-	// "log"
-	// "os"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	// "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func ConnectDataBase() {
 
-	// err := godotenv.Load(".env")
+	err := godotenv.Load(".env")
 
-	// if err != nil {
-	//   log.Fatalf("Error loading .env file")
-	// }
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 
 	Dbdriver := "mysql"
-	// DbHost := os.Getenv("DB_HOST")
-	// DbUser := os.Getenv("DB_USER")
-	// DbPassword := os.Getenv("DB_PASSWORD")
-	// DbName := os.Getenv("DB_NAME")
-	// DbPort := os.Getenv("DB_PORT")
+	DbHost := os.Getenv("DB_HOST")
+	DbUser := os.Getenv("DB_USER")
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DbName := os.Getenv("DB_NAME")
+	DbPort := os.Getenv("DB_PORT")
 
-	// DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
-	DBURL := "root:rootpass@tcp(0.0.0.0:3308)/scalabruh_final_proj_prod_db?charset=utf8&parseTime=True&loc=Local"
+	DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
+	// DBURL := "root:rootpass@tcp(0.0.0.0:3308)/scalabruh_final_proj_prod_db?charset=utf8&parseTime=True&loc=Local"
 
 	DB, _ = gorm.Open(Dbdriver, DBURL)
 
-	// if err != nil {
-	// 	fmt.Println("Cannot connect to database ", Dbdriver)
-	// 	log.Fatal("connection error:", err)
-	// } else {
-	// 	fmt.Println("We are connected to the database ", Dbdriver)
-	// }
+	if err != nil {
+		fmt.Println("Cannot connect to database ", Dbdriver)
+		log.Fatal("connection error:", err)
+	} else {
+		fmt.Println("We are connected to the database ", Dbdriver)
+	}
 
 	DB.DB().SetConnMaxLifetime(0)
 	DB.AutoMigrate(&Product{})
